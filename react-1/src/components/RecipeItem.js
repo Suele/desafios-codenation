775acbe9496f256dcd4231ca703e90ab67c48abd
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-04-28 20:45:17 -0300

    Adiciona os arquivos react-1 e react-2.

---
+++ react-1/src/components/RecipeItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const RecipeItem = ({ title, thumbnail, ingredients }) => (
+  <div className='card-deck'>
+    <div className='card' key={title}>
+      <img className='card-img-top' src={thumbnail} alt={title} />
+      <div className='card-body'>
+        <h5 className='card-title'>{title}</h5>
+        <p className='card-text'>
+          <strong>Ingredients: </strong>
+          {ingredients}
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default RecipeItem;
---
+++ react-1/src/components/RecipePage.js
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from "prop-types";
+import RecipeItem from "./RecipeItem";
+import { Link } from "react-router-dom";
+
+// TODO: Você deve verificar se a receita existe
+const RecipePage = ({ searchString = "", recipes = [] }) => {
+  return searchString
+    ? recipes.results
+        .filter(recipe => {
+          return (
+            recipe.title.toLowerCase().indexOf(searchString.toLowerCase()) !==
+              -1 ||
+            recipe.ingredients
+              .toLowerCase()
+              .indexOf(searchString.toLowerCase()) !== -1
+          );
+        })
+        .map(recipe => {
+          return (
+            <div className='col-sm-3 mt-2'>
+              <RecipeItem
+                key={recipe.title}
+                thumbnail={recipe.thumbnail}
+                title={recipe.title}
+                ingredients={recipe.ingredients}
+              />
+            </div>
+          );
+        })
+    : recipes.results.map(recipe => {
+        return (
+          <div className='col-sm-3 mt-2'>
+            <Link to={`/recipe/${recipe.title}`}>
+              <RecipeItem
+                key={recipe.title}
+                thumbnail={recipe.thumbnail}
+                title={recipe.title}
+                ingredients={recipe.ingredients}
+              />
+            </Link>
+          </div>
+        );
+      }) || <h1>Não foi Possivel</h1>;
+};
+
+RecipePage.propTypes = {
+  searchString: PropTypes.string,
+  recipe: PropTypes.object
+};
+
+export default RecipePage;
