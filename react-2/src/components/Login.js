c25bbfcd570a89921bae150e58a5354ba66e9cd8
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-04-29 23:49:05 -0300

    Adiciona as funcionalidades de login e registro de usuarios.

--- react-2/src/components/Login.js
+++ react-2/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import login from "../services/loginService";
+import loginService from "../services/loginService";
 
 class Login extends Component {
   constructor(props) {
@@ -10,10 +10,26 @@ class Login extends Component {
     };
   }
 
-  handlerSubmitForm = () => {};
+  loginUser = e => {
+    e.preventDefault();
+    const usuarioTentandoLogar = loginService.login(
+      this.state.username,
+      this.state.password
+    );
+    console.log(usuarioTentandoLogar);
+  };
+
+  registerUser = e => {
+    e.preventDefault();
+    const usuarioFazerRegistro = loginService.register(
+      this.state.username,
+      this.state.password
+    );
+    console.log(usuarioFazerRegistro);
+  };
 
   render = () => (
-    <form className='form-signin' onSubmit={this.handlerSubmitForm}>
+    <form className='form-signin'>
       <div className='text-center mb-4'>
         <h1 className='h3 mb-3 font-weight-normal'>Login / Register</h1>
       </div>
@@ -51,12 +67,14 @@ class Login extends Component {
 
       <div className='mt-5'>
         <button
+          onClick={this.loginUser}
           className='login btn btn-lg btn-primary btn-block'
           type='submit'
         >
           Login
         </button>
         <button
+          onClick={this.registerUser}
           className='register btn btn-lg btn-secondary btn-block'
           type='submit'
         >
