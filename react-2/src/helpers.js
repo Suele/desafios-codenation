70b88ede37687813c23aae35a85e084affab7257
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-02 05:19:54 -0300

    Adiciona a opção de remover a chave do localStorage.

--- react-2/src/helpers.js
+++ react-2/src/helpers.js
@@ -1,21 +1,20 @@
 const localStorageWrapper = {
   set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
-  get: (key) => {
-    const value = localStorage.getItem(key)
+  get: key => {
+    const value = localStorage.getItem(key);
 
-    return value ? JSON.parse(value) : value
-  }
-}
+    return value ? JSON.parse(value) : value;
+  },
+  remove: key => localStorage.removeItem(key)
+};
 
-const slugify = (str) => str
-  .toString()
-  .toLowerCase()
-  .replace(/\s+/g, '-')
-  .replace(/[^\w-]+/g, '')
-  .replace(/--+/g, '-')
-  .trim()
+const slugify = str =>
+  str
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .trim();
 
-export {
-    slugify,
-    localStorageWrapper
-}
+export { slugify, localStorageWrapper };
