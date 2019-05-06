2cf2136f30332fbdfdb66e7bd517bcc670917c88
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-05 23:09:41 -0300

    Adiciona a funcionalidade de inserir comentarios.

--- react-2/src/components/CommentsBlock.js
+++ react-2/src/components/CommentsBlock.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import commentsService from "../services/commentsService";
 import loginService from "../services/loginService";
 import { matchPath, withRouter } from "react-router";
+import { slugify } from "../helpers";
 
 class CommentsBlock extends Component {
   constructor(props) {
@@ -21,41 +22,48 @@ class CommentsBlock extends Component {
   addComment = e => {
     e.preventDefault();
     const { comments } = this.state;
+    const { match } = this.props;
+    const recipeSlug = slugify(match.params.recipeSlug);
 
+    const teste = commentsService.insert(recipeSlug, comments);
+    console.log("teste", teste);
+  };
+
+  renderComment = () => {
+    const { match } = this.props;
+    const recipeSlug = slugify(match.params.recipeSlug);
+    commentsService.get(recipeSlug).map(commentUser => {
+      return (
+        <div className='Comment media text-muted pt-3'>
+          <FontAwesomeIcon className='mr-2' size='3x' icon='user-circle' />
+          <p className='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'>
+            <strong className='d-block text-gray-dark'>
+              {console.log("author", commentUser.author)}
+              {console.log("date", commentUser.date)}
+            </strong>
+            {console.log("comentario", commentUser[0])}
+          </p>
+          {/* Icone deve aparecer somente quando o comentario for do usuario logado */}
+          {/* {console.log(commentsService.get(recipeSlug))} */}
+          <FontAwesomeIcon icon='trash' />
+        </div>
+      );
+    });
+  };
+
+  render() {
+    const { disabledComment } = this.state;
     const { pathname } = this.props.location;
+
     const match = matchPath(pathname, {
       path: "/recipe/:recipeSlug",
       exact: true,
       strict: false
     });
-
-    const teste = commentsService.insert(match.params, comments);
-    console.log("teste", teste);
-  };
-
-  renderComment = () => (
-    <div className='Comment media text-muted pt-3'>
-      <FontAwesomeIcon className='mr-2' size='3x' icon='user-circle' />
-      <p className='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'>
-        <strong className='d-block text-gray-dark'>@user</strong>
-        Comment
-      </p>
-      {/* Icone deve aparecer somente quando o comentario for do usuario logado */}
-      {console.log(commentsService.get())}
-      {console.log(loginService.isLogged())}
-      <FontAwesomeIcon icon='trash' />
-    </div>
-  );
-
-  render() {
-    const { disabledComment } = this.state;
-    const { match } = this.props;
     return (
       <div className='text-left'>
         <div className='my-3 p-3 bg-white rounded shadow-sm'>
           <h6 className='border-bottom border-gray pb-2 mb-0'>Comments</h6>
-          {this.renderComment()}
-          {this.renderComment()}
           {this.renderComment()}
         </div>
         <form>
@@ -66,7 +74,7 @@ class CommentsBlock extends Component {
               value={this.onChange}
               onChange={e => {
                 e.preventDefault();
-                this.setState({ comments: e.target.value });
+                this.setState({ comments: [].concat(e.target.value) });
               }}
               required='required'
               className='form-control'
