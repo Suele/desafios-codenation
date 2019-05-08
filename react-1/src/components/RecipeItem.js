4370ca2b4a5f981f7373987be77b803816e667c6
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-08 00:06:35 -0300

    refatora

--- react-1/src/components/RecipeItem.js
+++ react-1/src/components/RecipeItem.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { slugify } from "../helpers";
 
 const RecipeItem = ({ title, thumbnail, ingredients }) => (
   <Link to={`/recipe/${title}`}>
