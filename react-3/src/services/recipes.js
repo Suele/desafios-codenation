8a0a51a2c81c6cf3fea95341480ef8334867809c
Suele <suelen.ads.guimaraes@gmail.com>
2019-05-09 20:37:58 -0300

    Adiciona busca por nome de receita.

--- react-3/src/services/recipes.js
+++ react-3/src/services/recipes.js
@@ -1,16 +1,28 @@
-const API_PATH = 'http://localhost:3030/api'
+const API_PATH = "http://localhost:3030/api";
 
 const getRecipesByIngredients = (ingredients, page = 1) => {
-    // TODO implementar método
-    return []
-}
+  // TODO implementar método
+  return [];
+};
+const getRecipesByName = title => {
+  // TODO implementar método
 
-const getRecipesByName = (name = '', page = 1) => {
-    // TODO implementar método
-    return []
-}
+  fetch(API_PATH)
+    .then(response => response.json())
+    .then(recipes => {
+      recipes.results
+        .filter(recipe => {
+          return recipe.title.toLowerCase().indexOf(title.toLowerCase()) !== -1;
+        })
+        .map(recipe => {
+          console.log("recipe:", [recipe.title]);
+          return [recipe.title];
+        });
+      console.log("recipes1: ", recipes);
+      console.log("recipes2:", [recipes.results]);
+    });
+};
 
-export {
-    getRecipesByIngredients,
-    getRecipesByName
-}
+getRecipesByName("Ginger Champagne");
+
+export { getRecipesByIngredients, getRecipesByName };
