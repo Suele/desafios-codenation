260191713a6995c2b1051665301f5fc6c0f74be1
Suele <suelen.ads.guimaraes@gmail.com>
2019-05-09 21:04:22 -0300

    Adiciona busca por ingredientes das receitas.

--- react-3/src/services/recipes.js
+++ react-3/src/services/recipes.js
@@ -2,9 +2,20 @@ const API_PATH = "http://localhost:3030/api";
 
 const getRecipesByIngredients = (ingredients, page = 1) => {
   // TODO implementar método
-  return [];
+  fetch(API_PATH)
+  .then(response => response.json())
+  .then(recipes => {
+    recipes.results
+      .filter(recipe => {
+        return recipe.ingredients.toLowerCase().indexOf(ingredients.toLowerCase()) !== -1;
+      })
+      .map(recipe => {
+        console.log("recipe ingredients:", [recipe.ingredients]);
+        return [recipe.ingredients];
+      });
+  });
 };
-const getRecipesByName = title => {
+const getRecipesByName = (title, page=1) => {
   // TODO implementar método
 
   fetch(API_PATH)
@@ -15,7 +26,7 @@ const getRecipesByName = title => {
           return recipe.title.toLowerCase().indexOf(title.toLowerCase()) !== -1;
         })
         .map(recipe => {
-          console.log("recipe:", [recipe.title]);
+          console.log("recipe title:", [recipe.title]);
           return [recipe.title];
         });
       console.log("recipes1: ", recipes);
@@ -23,6 +34,7 @@ const getRecipesByName = title => {
     });
 };
 
+getRecipesByIngredients("sugar");
 getRecipesByName("Ginger Champagne");
 
 export { getRecipesByIngredients, getRecipesByName };
