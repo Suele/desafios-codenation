c9f43d6fc13ac0805b2a1059afd1ca372f0a8e38
Suele <suelen.ads.guimaraes@gmail.com>
2019-05-11 00:22:20 -0300

    Adiciona o return nas funções getRecipesByIngredients e getByName.

--- react-3/src/services/recipes.js
+++ react-3/src/services/recipes.js
@@ -2,35 +2,39 @@ const API_PATH = "http://localhost:3030/api";
 
 const getRecipesByIngredients = (ingredients, page = 1) => {
   // TODO implementar método
-  fetch(API_PATH)
-  .then(response => response.json())
-  .then(recipes => {
-    recipes.results
-      .filter(recipe => {
-        return recipe.ingredients.toLowerCase().indexOf(ingredients.toLowerCase()) !== -1;
-      })
-      .map(recipe => {
-        console.log("recipe ingredients:", [recipe.ingredients]);
-        return [recipe.ingredients];
-      });
-  });
-};
-const getRecipesByName = (title, page=1) => {
-  // TODO implementar método
-
-  fetch(API_PATH)
+  return fetch(API_PATH)
     .then(response => response.json())
     .then(recipes => {
-      recipes.results
+      return recipes.results
+        .filter(recipe => {
+          return (
+            recipe.ingredients
+              .toLowerCase()
+              .indexOf(ingredients.toLowerCase()) !== -1
+          );
+        })
+        .map(recipe => {
+          console.log("recipe ingredients:", [recipe.ingredients]);
+          return [recipe.ingredients];
+        });
+    });
+};
+const getRecipesByName = (title, page = 1) => {
+  // TODO implementar método
+
+  return fetch(API_PATH)
+    .then(response => response.json())
+    .then(recipes => {
+      return recipes.results
         .filter(recipe => {
           return recipe.title.toLowerCase().indexOf(title.toLowerCase()) !== -1;
         })
         .map(recipe => {
           console.log("recipe title:", [recipe.title]);
-          return [recipe.title];
+          return recipe.title;
         });
-      console.log("recipes1: ", recipes);
       console.log("recipes2:", [recipes.results]);
+      console.log("recipes1: ", recipes);
     });
 };
 
