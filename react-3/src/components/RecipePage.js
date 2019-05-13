2493bfa1072e52cee1a5dbaf8ce3d6e5cba51ac6
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-13 14:31:37 -0300

    Adiciona a seleção da receita selecionada.

--- react-3/src/components/RecipePage.js
+++ react-3/src/components/RecipePage.js
@@ -1,41 +1,61 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import RecipeItem from "./RecipeItem";
+import { getRecipesByName, getRecipesByIngredients } from "../services/recipes";
 
 class RecipePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      recipe: {
-        thumbnail: "",
-        title: "",
-        ingredients: ""
-      }
+      recipe: []
     };
   }
 
+  componentDidMount() {
+    const { page } = this.state;
+    const { match } = this.props;
+
+    if (match) {
+      getRecipesByName(match, page).then(results => {
+        this.setState({ recipe: results });
+        console.log("recipes: ", this.state.recipe);
+      });
+    }
+  }
+
   render() {
-    const { thumbnail, title, ingredients } = this.state.recipe;
+    const { recipe } = this.state;
 
     return (
       <div>
-        <img src={thumbnail} alt={title} />
-        <div className='card-body'>
-          <h5 className='card-title'>{title}</h5>
-          <p className='card-text'>
-            <strong>Ingredients: </strong>
-            {ingredients}
-          </p>
-          <h5 className='card-title'>Similar recipes</h5>
-          <div className='row'>
-            <RecipeItem />
-            <RecipeItem />
-            <RecipeItem />
-            <RecipeItem />
-          </div>
-        </div>
+        {recipe.map(selectedRecipe => {
+          return (
+            <div>
+              <img src={selectedRecipe.thumbnail} alt={selectedRecipe.title} />
+              <div className='card-body'>
+                <h5 className='card-title'>{selectedRecipe.title}</h5>
+                <p className='card-text'>
+                  <strong>Ingredients: </strong>
+                  {selectedRecipe.ingredients}
+                </p>
+                <h5 className='card-title'>Similar recipes</h5>
+                <div className='row'>
+                  <RecipeItem />
+                  <RecipeItem />
+                  <RecipeItem />
+                  <RecipeItem />
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
+
+RecipePage.propTypes = {
+  recipes: PropTypes.array
+};
 
 export default RecipePage;
