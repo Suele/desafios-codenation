aa207245ffceb9d367ea14f651f81ecd25a8cba1
Suele <suelen.ads.guimaraes@gmail.com>
2019-05-14 12:16:56 -0300

    Retira o filtro de pesquisa da API de receitas.

--- react-3/src/services/recipes.js
+++ react-3/src/services/recipes.js
@@ -1,42 +1,29 @@
 const API_PATH = "http://localhost:3030/api";
 
 const getRecipesByIngredients = (ingredients, page = 1) => {
-  // TODO implementar método
+
   return fetch(`${API_PATH}?i=${ingredients}&p=${page}`)
     .then(response => response.json())
     .then(recipes => {
       return recipes.results
-        .filter(recipe => {
-          return (
-            recipe.ingredients
-              .toLowerCase()
-              .indexOf(ingredients.toLowerCase()) !== -1
-          );
-        })
         .map(recipe => {
-          console.log("recipe ingredients:", recipe);
-          return recipe;
-        });
+            console.log("getRecipesByIngredients: ", recipe)
+            return recipe;
+        })
     });
 };
+
 const getRecipesByName = (title, page = 1) => {
-  // TODO implementar método
 
   return fetch(`${API_PATH}?q=${title}&p=${page}`)
     .then(response => response.json())
     .then(recipes => {
       return recipes.results
-        .filter(recipe => {
-          return recipe.title.toLowerCase().indexOf(title.toLowerCase()) !== -1;
-        })
         .map(recipe => {
-          console.log("recipe title:", recipe);
+          console.log("getRecipesByName: ", recipe)
           return recipe;
-        });
+        })
     });
 };
-
-console.log("teste ingredientes: ", getRecipesByIngredients("e", 1));
-console.log("teste ingredientes: ", getRecipesByName("ginger", 1));
 
 export { getRecipesByIngredients, getRecipesByName };
