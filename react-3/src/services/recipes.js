13b8d6d3cc43e4486d368267f935a49f84afa669
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-15 16:20:27 -0300

    Adiciona o catch para erros.

--- react-3/src/services/recipes.js
+++ react-3/src/services/recipes.js
@@ -1,29 +1,27 @@
 const API_PATH = "http://localhost:3030/api";
 
-const getRecipesByIngredients = (ingredients, page = 1) => {
-
+const getRecipesByIngredients = (ingredients, page) => {
   return fetch(`${API_PATH}?i=${ingredients}&p=${page}`)
     .then(response => response.json())
     .then(recipes => {
-      return recipes.results
-        .map(recipe => {
-            //console.log("getRecipesByIngredients: ", recipe)
-            return recipe;
-        })
-    });
+      return recipes.results.map(recipe => {
+        //console.log("getRecipesByIngredients: ", recipe)
+        return recipe;
+      });
+    })
+    .catch(error => console.log("error: ", error));
 };
 
-const getRecipesByName = (title, page = 1) => {
-
+const getRecipesByName = (title, page) => {
   return fetch(`${API_PATH}?q=${title}&p=${page}`)
     .then(response => response.json())
     .then(recipes => {
-      return recipes.results
-        .map(recipe => {
-          //console.log("getRecipesByName: ", recipe)
-          return recipe;
-        })
-    });
+      return recipes.results.map(recipe => {
+        //console.log("getRecipesByName: ", recipe)
+        return recipe;
+      });
+    })
+    .catch(error => console.log("error: ", error));
 };
 
 export { getRecipesByIngredients, getRecipesByName };
