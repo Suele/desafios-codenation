bdcdfcf1139863c8621fcf86bfc7793a4b903ad5
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-18 20:31:35 -0300

    Adiciona o logo da marvel e o estilo da nav conforme o modelo apresentado.

--- react-4/src/components/Nav/Nav.js
+++ react-4/src/components/Nav/Nav.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import marvelLogo from '../../img/marvel-logo.jpg';
+
+const Navbar = styled.nav`
+  padding: 2em;
+  margin: 0em;
+  background: black;
+`;
+
+const Img = styled.img`
+  border-radius: 8px;
+  width: 200px;
+`;
 
 const Nav = () => (
-    <h1> Nav</h1>
+  <Navbar>
+    <Img src={marvelLogo} alt="marvel-logo" />
+  </Navbar>
 );
 
 export default Nav;
