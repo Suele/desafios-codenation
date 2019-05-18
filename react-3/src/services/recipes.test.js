aecc5e41e2feae4674e8dcce1a0063b36b650c42
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-17 22:42:58 -0300

    Ajusta os testes para busca de receitas por nome ou ingrediente.

--- react-3/src/services/recipes.test.js
+++ react-3/src/services/recipes.test.js
@@ -1,15 +1,58 @@
 import { getRecipesByIngredients, getRecipesByName } from "./recipes";
-
 describe("RecipesService", () => {
-  test("getRecipesByName success", () => {
-    const result = getRecipesByName();
+  test("getRecipesByIngredients returns a Promise", () => {
+    const promise = getRecipesByIngredients().catch(err => {
+      /* ignore */
+    });
 
-    expect(result).toHaveLength(0);
+    expect(promise instanceof Promise).toBeTruthy();
   });
 
-  test("getRecipesByIngredients success", () => {
-    const result = getRecipesByIngredients();
+  test("getRecipesByName returns a Promise", () => {
+    const promise = getRecipesByName().catch(err => {
+      /* ignore */
+    });
 
-    expect(result).toHaveLength(0);
+    expect(promise instanceof Promise).toBeTruthy();
+  });
+
+  test("getRecipesByIngredients fetches a page", async () => {
+    const response = await getRecipesByIngredients().catch(err => {
+      /* ignore */
+    });
+
+    /**
+     * There are many ways in which the participant can
+     * implement this service. Thre recipies got from it
+     * can be in many paths. We need to consider as many
+     * as we can.
+     */
+
+    const recipes =
+      (response.data && response.data.results) || // axios returning results
+      response.data || // axios returning data
+      response.results || // fetch returning results
+      response || // axios or fetch returning response
+      expect(recipes).toHaveLength(10);
+  });
+
+  test("getRecipesByName fetches a page", async () => {
+    const response = await getRecipesByName().catch(err => {
+      /* ignore */
+    });
+
+    /**
+     * There are many ways in which the participant can
+     * implement this service. Thre recipies got from it
+     * can be in many paths. We need to consider as many
+     * as we can.
+     */
+
+    const recipes =
+      (response.data && response.data.results) || // axios returning results
+      response.data || // axios returning data
+      response.results || // fetch returning results
+      response || // axios or fetch returning response
+      expect(recipes).toHaveLength(10);
   });
 });
