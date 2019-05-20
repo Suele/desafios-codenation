ac28d7cffc576c9b4ed466ae91174fd53bac6d54
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-20 15:19:36 -0300

    Aumenta o tamanho do botão e o pocisiona abaixo da imagem.

--- react-4/src/components/Button/Button.js
+++ react-4/src/components/Button/Button.js
@@ -3,12 +3,17 @@ import styled from 'styled-components';
 
 const ButtonDetail = styled.button`
   color: palevioletred;
-  background: black
   font-size: 1em;
-  margin: 1em;
+  margin: 0 auto;
   padding: 0.25em 1em;
   border: 2px solid palevioletred;
-  border-radius: 3px;
+  border-radius: 5px;
+  font-size: 1.5em;
+  &:hover {
+    background-color: black;
+  }
+  width: 450px;
+  display: block;
 `;
 
 const Button = () => <ButtonDetail>Detalhes</ButtonDetail>;
