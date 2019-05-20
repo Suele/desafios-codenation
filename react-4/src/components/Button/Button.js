85d7a58520bf4deeff97e11be0a1805f034741b9
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-20 00:51:54 -0300

    Adiciona estilo para o botão de detalhes.

--- react-4/src/components/Button/Button.js
+++ react-4/src/components/Button/Button.js
@@ -1,1 +1,16 @@
-//TODO create button
+import React from 'react';
+import styled from 'styled-components';
+
+const ButtonDetail = styled.button`
+  color: palevioletred;
+  background: black
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const Button = () => <ButtonDetail>Detalhes</ButtonDetail>;
+
+export default Button;
