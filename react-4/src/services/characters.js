00d93adbd71fbc39293a42a891709d2f976c7ca7
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-22 13:11:24 -0300

    Adiciona as configurações para acessar a API.

--- react-4/src/services/characters.js
+++ react-4/src/services/characters.js
@@ -1,9 +1,18 @@
 import Api from '../utils/api';
-//publicKey: '48cad6d96d0302f3ce04409e00e49572',
+import moment from 'moment';
+import md5 from 'crypto-js/md5';
 
 class CharacterService {
   static getCharacters(payload) {
-    const { publicKey: '48cad6d96d0302f3ce04409e00e49572', timeStamp, hash, limit, name } = payload;
+    payload = {
+      publicKey: '48cad6d96d0302f3ce04409e00e49572',
+      privateKey: '50ff75690f39e084e3e09e5621a4fcc434f1440e',
+      timeStamp: moment().format(),
+      hash: md5((payload.timeStamp + payload.privateKey + payload.publicKey).toString()),
+      limit: '3000',
+      name: 'Project Marvel'
+    };
+    console.log(payload);
     return Api.get(`/v1/public/comics/${payload}`).then(results => console.log(results));
   }
 }
