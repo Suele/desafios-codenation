65af8f210ff1ccff01acceb06e5f35940b0d2520
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-21 23:15:30 -0300

    Adiciona a url  e as configurações de acesso.

--- react-4/src/services/characters.js
+++ react-4/src/services/characters.js
@@ -1,13 +1,11 @@
 import Api from '../utils/api';
+//publicKey: '48cad6d96d0302f3ce04409e00e49572',
 
 class CharacterService {
   static getCharacters(payload) {
-    const { publicKey, timeStamp, hash, limit, name } = payload;
-    return Api.get(
-     // TODO implementar 
-    );
+    const { publicKey: '48cad6d96d0302f3ce04409e00e49572', timeStamp, hash, limit, name } = payload;
+    return Api.get(`/v1/public/comics/${payload}`).then(results => console.log(results));
   }
 }
 
 export default CharacterService;
-
