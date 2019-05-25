d711d1b0b8ea16c6d9859bf818c4c75b7ba57315
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-25 02:14:58 -0300

    Adiciona export ao módulo.

--- react-4/src/config/config.js
+++ react-4/src/config/config.js
@@ -1,4 +1,8 @@
-export const marvelApi = {
+const marvelApi = {
   publicKey: '48cad6d96d0302f3ce04409e00e49572',
   privateKey: '50ff75690f39e084e3e09e5621a4fcc434f1440e'
+};
+
+export default {
+  marvelApi
 };
