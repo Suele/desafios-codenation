342a5ea21c75fd9d983ad052c7b26596dfb200ae
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-24 23:30:55 -0300

    Ajusta o acesso a API.

--- react-4/src/services/characters.js
+++ react-4/src/services/characters.js
@@ -1,19 +1,12 @@
 import Api from '../utils/api';
-import moment from 'moment';
-import md5 from 'crypto-js/md5';
 
 class CharacterService {
   static getCharacters(payload) {
-    payload = {
-      publicKey: '48cad6d96d0302f3ce04409e00e49572',
-      privateKey: '50ff75690f39e084e3e09e5621a4fcc434f1440e',
-      timeStamp: moment().format(),
-      hash: md5((payload.timeStamp + payload.privateKey + payload.publicKey).toString()),
-      limit: '3000',
-      name: 'Project Marvel'
-    };
-    console.log(payload);
-    return Api.get(`/v1/public/comics/${payload}`).then(results => console.log(results));
+    const query = Object.keys(payload).reduce((query, key) => {
+      return `${query}&${key}=${payload[key]}`;
+    }, '?');
+
+    return Api.get(`/v1/public/characters${query}`).then(res => res.data.data);
   }
 }
 
